85fe697ef50f1d1357497845e5572976e9bc395f
Hiago Adao <hiago@registro.rs>
2020-09-20 19:21:40 -0300

    feat: Implementando Controller para Atividade

---
+++ backend/teu_futuro/controllers/atividade.py
@@ -0,0 +1,21 @@
+from flask import jsonify, make_response
+from teu_futuro.services.atividade import AtividadeService
+from teu_futuro.schemas.atividade import AtividadeSchema
+
+
+class AtividadeController:
+    @staticmethod
+    def obter_todas_atividades(turma_id):
+        aluno_service = AtividadeService()
+        resp = aluno_service.obter_atividade_por_turma(turma_id)
+        return make_response(jsonify(resp), 200)
+
+    @staticmethod
+    def cadastrar_atividade(turma_id, request_body):
+        try:
+            dados_aluno = AtividadeSchema().load(request_body)
+            resp = AtividadeService().cadastrar_atividade_na_turma(
+                turma_id, dados_aluno)
+            return make_response(jsonify(resp), 200)
+        except BaseException as err:
+            return make_response(jsonify(err), 400)
