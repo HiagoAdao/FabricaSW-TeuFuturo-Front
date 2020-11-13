94d3bbc8a2960f880cc0b0eb018513bb379382fb
Hiago Adao <hiago@registro.rs>
2020-11-13 13:31:07 -0300

    update: Test deploy to develop.

--- src/pages/ListagemTurmas/index.js
+++ src/pages/ListagemTurmas/index.js
@@ -3,7 +3,7 @@ import { Container } from "./index.styled";
 const ListagemTurmas = (props) => {
   return (
     <Container>
-      <h1>Página de Listagem de Turmas</h1>
+      <h1>Página para Listagem de Turmas</h1>
     </Container>
   );
 };
