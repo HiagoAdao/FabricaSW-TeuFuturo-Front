a4e3b2c4ba9a79c6e22ada3bce65f64fa7f6df57
Hiago Adao <hiago@registro.rs>
2020-11-22 00:03:13 -0300

    fix: Removendo arquivo desnecessário.

--- src/components/index.js
+++
@@ -1,3 +0,0 @@
-// export {ResumeViewComponent} from 'dotzPayments/src/components/presentation/pix/resumeViewComponent';
-export { Table } from 'src/components/Table/index.js';
-export { Header } from 'src/components/Header/index.js';
